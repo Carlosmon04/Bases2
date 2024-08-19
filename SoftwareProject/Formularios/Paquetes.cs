6dc32d02f750ea112b2f0c906aa6de2c30646e02
Carlosmon04 <dcarlosmontoya04@gmail.com>
2024-08-18 20:06:40 -0600

    Primer Commit

---
+++ SoftwareProject/Formularios/Paquetes.cs
@@ -0,0 +1,144 @@
+﻿using SoftwareProject.Formularios.Formularios_de_DELETE;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SoftwareProject.Formularios
+{
+    public partial class Paquetes : Form
+    {
+        private SqlConnection cnx;
+        private SqlCommand cmd;
+        private SqlDataReader data;
+        private String Nombre;
+        private int Id,Horas;
+        private float Precio;
+        private int Cliente;
+
+
+        private void Paquetes_Load(object sender, EventArgs e)
+        {
+            CargarDatos();
+            txtNombre.Text = Nombre;
+            txtHoras.Text = Horas.ToString();
+            txtPrecio.Text = Precio.ToString();
+        }
+
+        public Paquetes(SqlConnection conexion,int cliente, int id, String nombre, float precio, int horas )
+        {
+            InitializeComponent();
+            cnx = conexion;
+            Cliente = cliente;
+            Nombre = nombre;
+            Id = id;
+            Precio = precio;
+            Horas = horas;
+        }
+
+
+
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+            Menu form1 = Application.OpenForms.OfType<Menu>().FirstOrDefault();
+
+            if (form1 != null)
+            {
+                form1.OpenChildForm(new VerPaquetes(cnx,Cliente));
+            }
+        }
+
+        private void btnAdquirir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "exec spAdquirirPaquete @Usuario, @Paquete, @Precio";
+                SqlCommand command = new SqlCommand(query, cnx);
+                command.Parameters.AddWithValue("@Usuario", Cliente);
+                command.Parameters.AddWithValue("@Paquete", Id);
+                command.Parameters.AddWithValue("@Precio", Precio);
+
+                command.ExecuteNonQuery();
+                MessageBox.Show("Paquete Adquirido con exito", "Completado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Menu form1 = Application.OpenForms.OfType<Menu>().FirstOrDefault();
+
+                if (form1 != null)
+                {
+                    form1.OpenChildForm(new VerPaquetes(cnx,Cliente));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrio un Error " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+           
+
+        }
+
+        private void CargarDatos()
+        {
+            try
+            {
+                string query = "exec spRecuperarServicios @ID";
+
+               
+ 
+                SqlCommand command = new SqlCommand(query, cnx);
+                command.Parameters.AddWithValue("@ID", Id);
+                SqlDataReader reader = command.ExecuteReader();
+
+                int yOffset = 5; 
+
+               
+                if (!this.Controls.Contains(panelLabels))
+                {
+                    this.Controls.Add(panelLabels);
+                    panelLabels.AutoScroll = true; 
+                }
+
+
+                while (reader.Read())
+                {
+                    string nombre = reader.GetString(0);
+
+                    Label nuevoLabel = new Label
+                    {
+                        Text = nombre,
+                        AutoSize = true,
+                        Location = new System.Drawing.Point(10, yOffset),
+                        Font = new Font("Cambria Math", 8, FontStyle.Bold)
+                    };
+
+                    
+                    panelLabels.Controls.Add(nuevoLabel);
+                    yOffset += nuevoLabel.Height - 10;
+                    nuevoLabel.ForeColor = Color.Black;
+                   
+                }
+
+                reader.Close();
+              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+    }
+}
+
---
+++ SoftwareProject/Formularios/VerPaquetes.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using SoftwareProject.Formularios.Inventario;
+
+namespace SoftwareProject.Formularios
+{
+    public partial class VerPaquetes : Form
+    {
+        SqlConnection cnx;
+        DataTable TabPaquetes;
+        int UsuarioID;
+        int clienteID;
+
+        public VerPaquetes(SqlConnection conexion, int usuarioID)
+        {
+            InitializeComponent();
+            cnx = conexion;
+            UsuarioID = usuarioID;
+        }
+
+        private void VerPaquetes_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                TabPaquetes = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter("select * from dbo.TablaPaquetes()", cnx);
+                adapter.Fill(TabPaquetes);
+                dataGridView1.DataSource = TabPaquetes;
+                dataGridView1.ReadOnly = true;
+                dataGridView1.AllowUserToAddRows = false;
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                dataGridView1.ScrollBars = ScrollBars.Both;
+                RecuperarClienteID();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocurrio un Error" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void RecuperarClienteID()
+        {
+            try
+            {
+                // Crear el comando SQL
+                SqlCommand command = new SqlCommand("SELECT dbo.RecuperarClienteID(@UserID) AS ClienteID", cnx);
+                command.Parameters.AddWithValue("@UserID", UsuarioID);
+
+                // Ejecutar el comando y obtener el SqlDataReader
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    // Verificar si el valor recuperado no es nulo antes de intentar leerlo
+                    if (!reader.IsDBNull(reader.GetOrdinal("ClienteID")))
+                    {
+                        clienteID = reader.GetInt32(reader.GetOrdinal("ClienteID"));
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("ClienteID es nulo o no se encontró.");
+                    }
+                }
+
+                // Cerrar el SqlDataReader
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al recuperar ClienteID: " + ex.Message);
+            }
+        }
+
+        private void txtBusqueda_TextChanged(object sender, EventArgs e)
+        {
+            string busqueda = txtBusqueda.Text;
+
+            string filtro = string.Format("Convert(Nombre, 'System.String') like '%{0}%'", busqueda);
+
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = filtro;
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Verifica si se ha seleccionado una fila
+            if (e.RowIndex >= 0 && e.RowIndex < dataGridView1.Rows.Count)
+            {
+                Menu form1 = Application.OpenForms.OfType<Menu>().FirstOrDefault();
+
+                try
+                {
+                    // Obtén el valor de la columna 'PaqueteId'
+                    object paqueteIdObj = TabPaquetes.DefaultView[e.RowIndex]["PaqueteId"];
+                    int paqueteId = Convert.ToInt32(paqueteIdObj);
+
+                    // Obtén el valor de la columna 'Nombre'
+                    string nombre = TabPaquetes.DefaultView[e.RowIndex]["Nombre"].ToString();
+
+                    // Obtén el valor de la columna 'Precio'
+                    object precioObj = TabPaquetes.DefaultView[e.RowIndex]["Precio"];
+                    float precio = Convert.ToSingle(precioObj);
+
+                    // Obtén el valor de la columna 'Horas'
+                    object horasObj = TabPaquetes.DefaultView[e.RowIndex]["CantidadHoras"];
+                    int horas = Convert.ToInt32(horasObj);
+
+                    // Verifica si el formulario 'form1' no es nulo antes de usarlo
+                    if (form1 != null)
+                    {
+                        form1.OpenChildForm(new Paquetes(cnx,clienteID, paqueteId, nombre, precio, horas));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Maneja la excepción y muestra un mensaje de error si es necesario
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
